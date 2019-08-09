65e4069ca746f459f9e987122b3fb047a0bc70c7
dfernandes <fernadj2@gmail.com>
2019-08-08 23:10:50 -0400

    fix ent func

--- R/df_to_entropy.R
+++ R/df_to_entropy.R
@@ -22,24 +22,21 @@ HCAdf_to_entropy = function(HCAdf,aggre.time=900,norm=TRUE,strict.input=F) {
          warning('Object is not of class HCAdf. Proceeding anyways but be careful...')
       }
    }
-   
-   HCAdf$dayvec=tvec_to_dayvec(HCAdf$t)
 
-   tvec = HCAdf$t
-   dounsampledtimevals = as.numeric(tvec) %/% aggre.time
-   ld = list(dounsampledtimevals)
-   nd = 1:length(dounsampledtimevals)
-   statemat = HCAdf[,!(colnames(HCAdf) %in% c('t','dayvec'))]
+    nc = ncol(HCAdf) ; cn = colnames(HCAdf)
+    HCAdf$dayvec = tvec_to_dayvec(HCAdf$t)
+    tvec = HCAdf$t
+    dounsampledtimevals = as.numeric(tvec)%/%aggre.time
+    ld = list(dounsampledtimevals)
+    nd = 1:length(dounsampledtimevals)
+    statemat = HCAdf[, !(colnames(HCAdf) %in% c("t", "dayvec"))]
+    aggre_summary = data.frame(t = aggregate(tvec, ld, mean)[, 
+        -1], aggregate(nd, ld, function(x) homeCageEntropy(x, 
+        statemat, norm = norm))[, -1])
+    colnames(aggre_summary) = gsub("_xd", "", cn)
 
-   aggre_summary = data.frame(
-        t=aggregate(tvec , ld , mean )[,-1],
-        aggregate(nd , ld, function(x) homeCageEntropy(x,statemat,norm=norm))[,-1]
-      )
-   colnames(aggre_summary)[-1]=gsub('_xd','',colnames(statemat))   
-
-   aggre_summary$dayvec = tvec_to_dayvec(aggre_summary$t)
-
-   class(aggre_summary) = c('HCAentropy',class(aggre_summary))
-   return(aggre_summary)   
+    aggre_summary$dayvec = tvec_to_dayvec(aggre_summary$t)
+    class(aggre_summary) = c("HCAentropy", class(aggre_summary))
+    return(aggre_summary)
 }
 
