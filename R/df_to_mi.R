27a44327fe2b8d9b08ce4d6361d17718b08d759e
dfernandes <fernadj2@gmail.com>
2019-08-08 23:18:42 -0400

    fix MI func

--- R/df_to_mi.R
+++ R/df_to_mi.R
@@ -22,15 +22,13 @@ HCAdf_to_MI = function(HCAdf,aggre.time=900,norm=TRUE,strict.input=F) {
          warning('Object is not of class HCAdf. Proceeding anyways but be careful...')
       }
    }
-   
+   nc = ncol(HCAdf) ; cn = colnames(HCAdf)
    HCAdf$dayvec=tvec_to_dayvec(HCAdf$t)
-
    tvec = HCAdf$t
    dounsampledtimevals = as.numeric(tvec) %/% aggre.time
    ld = list(dounsampledtimevals)
    nd = 1:length(dounsampledtimevals)
    statemat = HCAdf[,!(colnames(HCAdf) %in% c('t','dayvec'))]
-
    aggre_summary = data.frame(
         t=aggregate(tvec , ld , mean )[,-1],
         aggregate(nd , ld, function(x) condMI(x,statemat,norm=norm))[,-1]
@@ -38,7 +36,6 @@ HCAdf_to_MI = function(HCAdf,aggre.time=900,norm=TRUE,strict.input=F) {
    colnames(aggre_summary)[-1]=gsub('_xd','',colnames(statemat))   
 
    aggre_summary$dayvec = tvec_to_dayvec(aggre_summary$t)
-
    class(aggre_summary) = c('HCAmi',class(aggre_summary))
    return(aggre_summary)   
 }
