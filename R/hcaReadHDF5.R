fbede6620f6f193e6840769a488f57f44411e5be
darren <fernadj2@gmail.com>
2025-01-22 20:37:41 -0500

    read hdf5

--- R/hcaReadHDF5.R
+++ R/hcaReadHDF5.R
@@ -1,9 +1,10 @@
-#' Read a HCA HDF5 file
+#' Read a HCA HDF5 file using rhdf5
 #'
 #' Read an HCA HDF5 file and return a list of dataframes, one for each subjects
 #' @param i input HDF5 file
 #' @param subjectIDs List of subjects IDs. If NULL, the subjects are automatically determined
 #' @param start.time The start time of HCA HDF5 file. If NULL, it is automatically determined.
+#' @param downsample If TRUE, Aggregate data by second (Default). If FALSE, don't aggregate. 
 #' @return List of dataframes, one for each subjects.
 #' @import rhdf5
 #' @export
@@ -12,7 +13,7 @@
 #'   hcaReadHDF5('1547743375205_000095_arbC8123198@LT-GREEN_0000085500200.hdf5')
 #' }
 
-hcaReadHDF5=function(i,subjectIDs = NULL,start.time = NULL) {
+hcaReadHDF5=function(i,subjectIDs = NULL,start.time = NULL, downsample = T) {
 
    if (is.null(start.time)) {
       start.time=find_start_time(i)
@@ -25,18 +26,30 @@ hcaReadHDF5=function(i,subjectIDs = NULL,start.time = NULL) {
    filedatalistsubject=list()     
    for (j in subjectIDs) {
      if (!j %in% subjects_in_this_file) {next}
-     subjaddress = paste0('/subjects/',j,'/antenna/ant1/')  #address in hdf5file for subject
      
-     subjdat = suppressWarnings({h5read(i,subjaddress)})[,c("bx","by",'t','antenna')]   #extract info
+     #address in hdf5file for subject
+     subjaddress = paste0('/subjects/',j,'/antenna/ant1/')  
+     
+     #extract info
+     subjdat = suppressWarnings({h5read(i,subjaddress)})[,c("bx","by",'t','antenna')]   
+     
+     # convert time from seconds to milliseconds 
+     subjdat[,'t'] = subjdat[,'t']/1000
 
-     tds = subjdat$t %/% 1000   # unionize by seconds
-     subjdatds = data.frame( t = aggregate(subjdat[,'t'],list(tds),mean)[,'x']/1000,   #time in seconds
-                             bx = aggregate(subjdat[,'bx'],list(tds),mean)[,'x'],
-                             by = aggregate(subjdat[,'by'],list(tds),mean)[,'x'],
-                             antenna = aggregate(subjdat[,'antenna'],list(tds),Mode)[,'x']
-                           )
-     subjdatds$t = subjdatds$t + start.time   #add starttime
-     filedatalistsubject[j] = list(subjdatds)
+     if (downsample) {
+        tds = floor(subjdat$t)      # unionize by seconds
+        subjdatds = data.frame( 
+           t = aggregate(subjdat[,'t'],list(tds),mean)[,'x'],   #time in seconds
+           bx = aggregate(subjdat[,'bx'],list(tds),mean)[,'x'],
+           by = aggregate(subjdat[,'by'],list(tds),mean)[,'x'],
+           antenna = aggregate(subjdat[,'antenna'],list(tds),Mode)[,'x']
+         )
+        subjdat = subjdatds
+     }
+     
+     subjdat$t = subjdat$t + start.time   #add starttime
+     
+     filedatalistsubject[j] = list(subjdat)
    }
 #   H5close()
    return(filedatalistsubject)
